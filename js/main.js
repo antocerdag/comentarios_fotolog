088d464e7b1b0b6b9b6af53e88614a42bafcddc7
Antonia Cerda <antoniacerdag@gmail.com>
2017-06-16 18:44:06 -0400

    editando btnes y js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 /*aquí va tu código*/
+
+function tomarDatos(){
+	 // Paso 1. llamo el botón y le doy la función
+
+	 var boton = document.getElementById("botonGuardar");
+
+	 botonGuardar.addEventListener("click" , tomardatos);
+}
+ tomarDatos();
